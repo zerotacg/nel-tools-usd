a646f9da10247d42baf6ffc8f1465c30e0e7e767
Tobias Peters <tobias.peters@kreativeffekt.at>
2026-05-10 11:49:27 +0200

    write empty shape file

--- app-usd-to-mesh/src/convert/Converter.cpp
+++ app-usd-to-mesh/src/convert/Converter.cpp
@@ -1,32 +1,7 @@
 module;
-#include <string>
-#include <fmt/color.h>
-#include <fmt/format.h>
-#include <nel/3d/material.h>
+#include <memory>
+#include <nel/3d/mesh_base.h>
 #include <nel/3d/mesh.h>
-#include <nel/3d/texture.h>
-#include <nel/3d/texture_cube.h>
-#include <nel/3d/texture_file.h>
-#include <nel/3d/texture_multi_file.h>
-#include <nel/misc/common.h>
-#include <nel/misc/path.h>
-#include <pxr/base/gf/vec2f.h>
-#include <pxr/base/gf/vec3f.h>
-#include <pxr/usd/kind/registry.h>
-#include <pxr/usd/usd/modelAPI.h>
-#include <pxr/usd/usdGeom/mesh.h>
-#include <pxr/usd/usdGeom/metrics.h>
-#include <pxr/usd/usdGeom/modelAPI.h>
-#include <pxr/usd/usdGeom/primvarsAPI.h>
-#include <pxr/usd/usdGeom/scope.h>
-#include <pxr/usd/usdGeom/subset.h>
-#include <pxr/usd/usdGeom/tokens.h>
-#include <pxr/usd/usdGeom/xform.h>
-#include <pxr/usd/usdHydra/tokens.h>
-#include <pxr/usd/usdShade/material.h>
-#include <pxr/usd/usdShade/materialBindingAPI.h>
-#include <pxr/usd/usdShade/shader.h>
-#include <pxr/usd/usdShade/tokens.h>
 
 module nel_tools.usd.usd_to_mesh.convert.Converter;
 import nel_tools.usd.convert;
@@ -38,10 +13,21 @@ namespace nel_tools::usd::usd_to_mesh::convert
     using namespace NL3D;
     using namespace NLMISC;
     using namespace std;
-    using namespace pxr;
 
     auto Converter::convert() -> unique_ptr<IShape>
     {
-        return nullptr;
+
+        auto target = make_unique<CMesh>();
+        CMeshBase::CMeshBaseBuild buildBaseMesh;
+        // todo implement mesh base build
+        // @see CExportNel::buildBaseMeshInterface (buildBaseMesh, maxBaseBuild, node, time, nodeMatrix);
+
+        // todo implement mesh build
+        CMesh::CMeshBuild buildMesh;
+        //@see CExportNel::buildMeshInterface (*tri, buildMesh, buildBaseMesh, maxBaseBuild, node, time, nodeMap);
+
+        target->build(buildBaseMesh, buildMesh);
+        //target->optimizeMaterialUsage();
+        return target;
     }
 }
