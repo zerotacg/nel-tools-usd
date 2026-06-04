a97fa00bbef53f7a91a627a2955132af719ce2b2
Tobias Peters <tobias.peters@kreativeffekt.at>
2026-06-04 22:13:41 +0200

    remove unused imports

--- app-shape-to-usd/src/convert/mesh/ConverterCMeshMRMSkinned.cpp
+++ app-shape-to-usd/src/convert/mesh/ConverterCMeshMRMSkinned.cpp
@@ -1,30 +1,21 @@
 module;
 #include <string>
 #include <ranges>
-#include <fmt/color.h>
 #include <fmt/format.h>
-#include <nel/3d/material.h>
 #include <nel/3d/mesh_mrm_skinned.h>
-#include <nel/3d/texture.h>
 #include <nel/misc/common.h>
 #include <nel/misc/path.h>
-#include <pxr/base/gf/vec2f.h>
-#include <pxr/base/gf/vec3f.h>
 #include <pxr/usd/kind/registry.h>
 #include <pxr/usd/usd/modelAPI.h>
 #include <pxr/usd/usdGeom/mesh.h>
-#include <pxr/usd/usdGeom/metrics.h>
 #include <pxr/usd/usdGeom/modelAPI.h>
-#include <pxr/usd/usdGeom/primvarsAPI.h>
 #include <pxr/usd/usdGeom/scope.h>
 #include <pxr/usd/usdGeom/subset.h>
 #include <pxr/usd/usdGeom/tokens.h>
-#include <pxr/usd/usdGeom/xform.h>
 #include <pxr/usd/usdGeom/xformCommonAPI.h>
 #include <pxr/usd/usdHydra/tokens.h>
 #include <pxr/usd/usdShade/material.h>
 #include <pxr/usd/usdShade/materialBindingAPI.h>
-#include <pxr/usd/usdShade/shader.h>
 #include <pxr/usd/usdShade/tokens.h>
 
 module nel_tools.usd.shape_to_usd.convert.mesh.ConverterCMeshMRMSkinned;
